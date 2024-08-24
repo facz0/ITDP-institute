f323f112d26bbef230c21d2bdf242100f3738041
mormi08 <fabriziocossio99@gmail.com>
2024-08-24 13:30:56 -0500

    aplicando estilos

--- JS/bienvenido.js
+++ JS/bienvenido.js
@@ -4,49 +4,56 @@ let c1 = {
     "class2": "carrera-fade",
     "class3": "carrera-content",
     "title": "Administración y Sistemas",
-    "profesor": "Laura Méndez"
+    "profesor": "Laura Méndez",
+    "id_curso": "A1B2C3"
 }
 let c2 = {
-    "class1": "carrera2",
+    "class1": "carrera",
     "class2": "carrera-fade",
     "class3": "carrera-content",
     "title": "Computación e informática",
-    "profesor": "Carlos Ruiz"
+    "profesor": "Carlos Ruiz",
+    "id_curso": "4D5E6F"
 }
 let c3 = {
-    "class1": "carrera3",
+    "class1": "carrera",
     "class2": "carrera-fade",
     "class3": "carrera-content",
     "title": "Electricidad Industrial",
-    "profesor": "Mariana López"
+    "profesor": "Mariana López",
+    "id_curso": "G7H8I9"
 }
 let c4 = {
-    "class1": "carrera4",
+    "class1": "carrera",
     "class2": "carrera-fade",
     "class3": "carrera-content",
     "title": "Dibujo Arquitectónico",
-    "profesor": "Andrés García"
+    "profesor": "Andrés García",
+    "id_curso": "J0K1L2"
 }
 let c5 = {
-    "class1": "carrera5",
+    "class1": "carrera",
     "class2": "carrera-fade",
     "class3": "carrera-content",
     "title": "Mecánica Automotríz",
-    "profesor": "Sofía Fernández"
+    "profesor": "Sofía Fernández",
+    "id_curso": "M3N4O5"
 }
 let c6 = {
-    "class1": "carrera6",
+    "class1": "carrera",
     "class2": "carrera-fade",
     "class3": "carrera-content",
     "title": "Diseño Gráfico",
-    "profesor": "Ricardo Torres"
+    "profesor": "Ricardo Torres",
+    "id_curso": "P6Q7R8"
 }
 let c7 = {
-    "class1": "carrera7",
+    "class1": "carrera",
     "class2": "carrera-fade",
     "class3": "carrera-content",
     "title": "Ingeniería Electrónica",
-    "profesor": "Patricia Morales"
+    "profesor": "Patricia Morales",
+    "id_curso": "S9T0U1"
 }
 carreras.push(c1);
 carreras.push(c2);
@@ -66,8 +73,9 @@ let cargarCarreras = ()=>{
         <section class="${i.class1}">
             <div class="${i.class2}">
                 <div class="${i.class3}">
-                    <h3>${i.title}</h3>
-                    <p>Profesor: ${i.profesor}</p>
+                    <p class="cod_curso">Cófigo: ${i.id_curso}</p>
+                    <h3 class="titulo_curso">${i.title}</h3>
+                    <p class="info_curso">En curso  |  Profesor: ${i.profesor}</p>
                 </div>
             </div>
         </section>
